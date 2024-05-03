7d5789c437010304171321b990f36bb9bdb2bb09
Diego Neri <diego@diegoneri.com>
2024-05-02 21:00:19 -0300

    Ajusta classe Conta

--- src/main/java/com/br/fatecrl/conta/model/Conta.java
+++ src/main/java/com/br/fatecrl/conta/model/Conta.java
@@ -1,22 +1,14 @@
 package com.br.fatecrl.conta.model;
 
-import java.io.Serializable;
-import java.util.Objects;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_conta")
-public class Conta extends AbstractEntity implements Serializable {
+public class Conta extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+
 	@Column(name = "nr_agencia", nullable = false)
 	private Integer agencia;
 	@Column(name = "nm_numero", nullable = false, length = 10)
@@ -31,15 +23,7 @@ public class Conta extends AbstractEntity implements Serializable {
 	}
 
 	public Conta(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
+		super.setId(id);
 	}
 
 	public Integer getAgencia() {
@@ -72,23 +56,6 @@ public class Conta extends AbstractEntity implements Serializable {
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Conta other = (Conta) obj;
-		return Objects.equals(id, other.id);
 	}
 
 }
